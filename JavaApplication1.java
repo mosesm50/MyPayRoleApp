6117532cbac33221376182887c043d33e5844d7d
Sinyoro <105461627+mosesm50@users.noreply.github.com>
2023-06-19 14:42:22 +0200

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author MOYOM
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Netbeans 17");
+        // TODO code application logic here
+    }
+    
+}
